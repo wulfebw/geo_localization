d0d40cc8b8c2c20ebcfa1ccd8a13cb4254209106
Blake Warren Wulfe <wulfebw@corn11.stanford.edu>
2016-02-14 22:02:41 -0800

    linear classification

---
+++ scripts/data_utils.py
@@ -0,0 +1,154 @@
+"""
+:description: Utilities for loading in, preprocessing, and organizing data.
+"""
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import scipy.io
+
+# filenames and locations
+DATA_DIR = '../data'
+CRCV_DIR = '/crcv'
+OUTPUTS_DIR = '../outputs'
+LABLES_FILENAME = 'GPS_Long_Lat_Compass.mat'
+HIST_FILENAME = 'Color_hist.mat'
+GIST_FILENAME = 'GIST.mat'
+
+# data constants
+TRAIN_RATIO = .8
+VAL_RATIO = .1
+TEST_RATIO = .1
+NUM_HIST_FEATURES = 60
+NUM_HIST_ROWS_PER_LOCATION = 5
+
+# latitude and longitude min and max for each city
+ORLANDO_LAT_MIN = 28
+ORLANDO_LAT_MAX = 29
+ORLANDO_LONG_MIN = -82
+ORLANDO_LONG_MAX = -81
+NYC_LAT_MIN = 40
+NYC_LAT_MAX = 42
+NYC_LONG_MIN = -75
+NYC_LONG_MAX = -73
+PITT_LAT_MIN = 40
+PITT_LAT_MAX = 42
+PITT_LONG_MIN = -81
+PITT_LONG_MAX = -79
+
+def load_mat(filename):
+    """
+    :description: Load and return the dictionary in a matlab file
+    """
+    assert filename.endswith('.mat'), 'load_mat requires .mat file'
+    arr = scipy.io.loadmat(os.path.join(DATA_DIR, filename))
+    return arr
+
+def load_labels():
+    """
+    :description: Loads labels for each row in the dataset. Labels returned
+        as a list of values where the index of the value is the XXXXXXX row
+        of the corresponding image and the label itself is one of
+        {1 = orlando, 2 = nyc, 3 = pitt}.
+    """
+    gps = load_mat(LABLES_FILENAME)['GPS_Compass']
+   
+    # collect the row numbers that correspond to each city
+    # 1 = orlando, 2 = nyc, 3 = pitt
+    labels = []
+    for idx, (latitude, longitude, angle) in enumerate(gps):
+
+        # orlando
+        if latitude > ORLANDO_LAT_MIN and latitude < ORLANDO_LAT_MAX \
+                and longitude > ORLANDO_LONG_MIN and longitude < ORLANDO_LONG_MAX:
+            labels.append((idx, 1))
+
+        # nyc
+        elif latitude > NYC_LAT_MIN and latitude < NYC_LAT_MAX \
+                and longitude > NYC_LONG_MIN and longitude < NYC_LONG_MAX:
+            labels.append((idx, 2))
+
+        # pitt
+        elif latitude > PITT_LAT_MIN and latitude < PITT_LAT_MAX \
+                and longitude > PITT_LONG_MIN and longitude < PITT_LONG_MAX:
+            labels.append((idx, 3))
+
+        # invalid latitude or longitude
+        else:
+            raise ValueError('Latitude or longitude not in valid range.\
+                               Longitude: {}\tLatitude: {}'.format(latitude, longitude))
+
+    # return the label corresponding to each row
+    return np.array(labels)[:, 1]
+
+def load_features_and_labels(feature_set='all'):
+    """
+    :description: load features and corresponding labels
+
+    :type feature_set: string
+    :param feature_set: Which features to load. One of {'all', 'hist', 'gist'}
+    """
+    
+    # load the labels with the features so we can randomly permute the order
+    labels = load_labels()
+
+    # repeat the labels for each location
+    labels = np.tile(labels, (NUM_HIST_ROWS_PER_LOCATION, 1)).T.reshape(-1)
+        
+    # load features
+    if feature_set == 'all':
+        hist = load_mat(HIST_FILENAME)['color_hist']
+        gist = load_mat(GIST_FILENAME)['RefImGIST']
+        feats = np.hstack((hist, gist))
+    elif feature_set == 'hist':
+        feats = load_mat(HIST_FILENAME)['color_hist']
+    elif feature_set == 'gist':
+        feats = load_mat(GIST_FILENAME)['RefImGIST']
+    else:
+        raise ValueError("""feature_set must be one of {'all', 'hist', 'gist'}""")
+    
+    # remove all zero rows - rows with data are separated by 5 all zero
+    # rows, i'm not sure why but remove them here
+    feats = feats[np.any(feats != 0, axis=1)]
+
+    # permute features and labels
+    # we want the test set to always be the same images, so set random seed before permuting
+    np.random.seed(42)
+    idxs = np.random.permutation(len(feats))
+    feats = feats[idxs]
+    labels = labels[idxs]
+
+    # train, val, test split
+    train_idx = int(len(feats) * TRAIN_RATIO)
+    val_idx = int(len(feats) * (TRAIN_RATIO + VAL_RATIO))
+    train = feats[:train_idx]
+    y_train = labels[:train_idx]
+    val = feats[train_idx: val_idx]
+    y_val = labels[train_idx: val_idx]
+    test = feats[val_idx:]
+    y_test = labels[val_idx:] 
+        
+    # compute mean and std on train data
+    means = np.mean(train, axis=0)
+    stds = np.std(train, axis=0)
+
+    # and use those stats to normalize train, val, test
+    train = (train - means) / stds
+    val = (val - means) / stds
+    test = (test - means) / stds
+
+    return train, y_train, val, y_val, test, y_test
+
+def load_crcv():
+    pass
+
+if __name__ == '__main__':
+    train, y_train, val, y_val, test, y_test = load_features_and_labels()
+    print train.shape
+    print y_train.shape
+    print val.shape
+    print y_val.shape
+    print test.shape
+    print y_test.shape
+    
